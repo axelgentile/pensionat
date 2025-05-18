480461b702838dbf938ce2f597d8073be53e9a60
axelgentile <gentileaxel466@gmail.com>
2025-05-18 15:26:54 +0200

    dependencies tillagda

---
+++ src/main/java/org/example/pensionatbackend1/PensionatBackend1Application.java
@@ -0,0 +1,13 @@
+package org.example.pensionatbackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatBackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensionatBackend1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pensionatbackend1/PensionatBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pensionatbackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionatBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
