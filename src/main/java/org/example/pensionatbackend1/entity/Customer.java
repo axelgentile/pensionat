cfdf4f8079042a0ef7de0912063974903b493f06
Lucas Frykman <lucasfrykmaan@gmail.com>
2025-05-19 19:58:00 +0200

    added connection to db, and created id for customer

--- src/main/java/org/example/pensionatbackend1/entity/Customer.java
+++ src/main/java/org/example/pensionatbackend1/entity/Customer.java
@@ -1,4 +1,18 @@
 package org.example.pensionatbackend1.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+    @GeneratedValue
+    @Id
+    private Long id;
 }
