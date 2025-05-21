9e4804f8c5320fea00cf0d44a6d83f072701b1ea
noelnp <noel.peltomaki@gmail.com>
2025-05-21 13:42:23 +0200

    Ground setup CustomerController

--- src/main/java/org/example/pensionatbackend1/controller/CustomerController.java
+++ src/main/java/org/example/pensionatbackend1/controller/CustomerController.java
@@ -1,4 +1,22 @@
 package org.example.pensionatbackend1.controller;
 
+import org.example.pensionatbackend1.entity.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @GetMapping("/all")
+    public List<Customer> getAllCustomers() {
+        return new ArrayList<>();
+    }
+
+
+
 }
