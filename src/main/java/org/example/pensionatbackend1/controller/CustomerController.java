769dc7ff3a4e9950a9ce909fdf20ab21160de826
noelnp <noel.peltomaki@gmail.com>
2025-05-21 15:11:37 +0200

    Add methods on CustomerService and Controller

--- src/main/java/org/example/pensionatbackend1/controller/CustomerController.java
+++ src/main/java/org/example/pensionatbackend1/controller/CustomerController.java
@@ -3,14 +3,16 @@ package org.example.pensionatbackend1.controller;
 import org.apache.coyote.Response;
 import org.example.pensionatbackend1.entity.Customer;
 import org.example.pensionatbackend1.service.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -24,11 +26,16 @@ public class CustomerController {
         return customerService.getAllCustomers();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
+        Customer customer = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customer);
+    }
+
     @PostMapping("/register")
     public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer) {
 
-        return null;
-
+        Customer saved = customerService.createCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
-
 }
--- src/main/java/org/example/pensionatbackend1/service/CustomerService.java
+++ src/main/java/org/example/pensionatbackend1/service/CustomerService.java
@@ -5,18 +5,32 @@ import org.example.pensionatbackend1.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 
 @Service
 public class CustomerService {
 
     private final CustomerRepository repository;
+    private final CustomerRepository customerRepository;
 
-    public CustomerService(CustomerRepository repository) {
+    public CustomerService(CustomerRepository repository, CustomerRepository customerRepository) {
         this.repository = repository;
+        this.customerRepository = customerRepository;
     }
 
     public List<Customer> getAllCustomers() {
         return repository.findAll();
+    }
+
+    public Customer getCustomerById(long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException());
+
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return repository.save(customer);
     }
 }
