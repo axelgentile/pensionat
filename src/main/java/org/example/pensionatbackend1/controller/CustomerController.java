73e9562dd73858212fae106192999345201b23c5
axelgentile <gentileaxel466@gmail.com>
2025-05-21 15:52:43 +0200

    dto och mapper kopplade till customercontroller

--- src/main/java/org/example/pensionatbackend1/controller/CustomerController.java
+++ src/main/java/org/example/pensionatbackend1/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package org.example.pensionatbackend1.controller;
 
 import org.apache.coyote.Response;
+import org.example.pensionatbackend1.dto.CustomerDto;
 import org.example.pensionatbackend1.entity.Customer;
+import org.example.pensionatbackend1.mapper.CustomerMapper;
 import org.example.pensionatbackend1.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customers")
@@ -22,20 +25,21 @@ public class CustomerController {
     }
 
     @GetMapping("/all")
-    public List<Customer> getAllCustomers() {
-        return customerService.getAllCustomers();
+    public List<CustomerDto> getAllCustomers() {
+        return customerService.getAllCustomers().stream().map(CustomerMapper::toDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable long id) {
         Customer customer = customerService.getCustomerById(id);
-        return ResponseEntity.ok(customer);
+        return ResponseEntity.ok(CustomerMapper.toDto(customer));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer) {
+    public ResponseEntity<CustomerDto> registerCustomer(@RequestBody CustomerDto customerDto) {
 
+        Customer customer = CustomerMapper.toEntity(customerDto);
         Customer saved = customerService.createCustomer(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CustomerMapper.toDto(saved));
     }
 }
