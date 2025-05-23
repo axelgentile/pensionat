6f22aba9a92cdcc9fe2deb5297844d783a6891f4
noelnp <noel.peltomaki@gmail.com>
2025-05-23 10:43:33 +0200

    Testing branch setup

---
+++ src/test/java/org/example/pensionatbackend1/RoomControllerTest.java
@@ -0,0 +1,7 @@
+package org.example.pensionatbackend1;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RoomControllerTest {
+}
---
+++ src/test/java/org/example/pensionatbackend1/RoomServiceTest.java
@@ -0,0 +1,8 @@
+package org.example.pensionatbackend1;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RoomServiceTest {
+
+}
